e881296136c9af4abded201ea0f8eca83e44e174
MinSung Kwon <csstumings804@gmail.com>
2025-11-09 23:54:21 +0900

    사진 업로드시 허용할 최대 body크기 설정

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      // 여기서 서버액션으로 허용할 최대 body 크기 설정
+      // 문자열 포맷: '2mb', '10mb', '50mb' 등
+      bodySizeLimit: "20mb",
+    },
+  },
 };
 
 export default nextConfig;
