ded7f105d2e1608ade9430dc370b6b5260a28298
MinSung Kwon <csstumings804@gmail.com>
2025-11-12 01:59:03 +0900

    Signup action 추가

--- src/app/action/auth.ts
+++ src/app/action/auth.ts
@@ -28,3 +28,22 @@ export async function logoutAction() {
   await supabase.auth.signOut();
   redirect("/");
 }
+
+export async function SignupAction(formData: FormData){
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+  
+  const supabase = await createClient();
+
+  const {error} = await supabase.auth.signUp({
+    email,
+    password,
+  })
+
+  if (error) {
+    console.error("회원가입 실패: ", error.message);
+    throw new Error(error.message);
+  }
+  console.log("회원가입 성공", email);
+  redirect("/login");
+}
